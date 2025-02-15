41851e690451ae6c5a450742366b65e56b75a4b2
MD Aftab Uddin Bhuiyan <149228564+aftab-uddin10@users.noreply.github.com>
2025-02-15 16:25:39 +0600

    01-git command .asm

---
+++ 01-git command .asm
@@ -0,0 +1,12 @@
+…or create a new repository on the command line
+echo "# Microprocessor-and-Assembly-Language" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/aftab-uddin10/Microprocessor-and-Assembly-Language.git
+git push -u origin main
+…or push an existing repository from the command line
+git remote add origin https://github.com/aftab-uddin10/Microprocessor-and-Assembly-Language.git
+git branch -M main
+git push -u origin main
